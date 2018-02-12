3383a0c950dbfbdc4aa90aabf9427ad602988a5d
madman1 <taras.plavin@gmail.com>
2018-02-12 22:51:00 +0200

    add comments to quick sort

--- Algorithms/Sorting/QuickSorter.cs
+++ Algorithms/Sorting/QuickSorter.cs
@@ -8,11 +8,20 @@ namespace Algorithms.Sorting
     {
         #region Quick sort for int
 
+        /// <summary>
+        /// Quick sort of array
+        /// </summary>
         public static void QuickSort(IList<int> arr)
         {
             QuickSort(arr, 0, arr.Count - 1);
         }
 
+        /// <summary>
+        /// Recursive quick sort implementation
+        /// </summary>
+        /// <param name="arr">Initial array</param>
+        /// <param name="low">Low index of array from which it should be sorted</param>
+        /// <param name="high">High index of array till which it should be sorted</param>
         public static void QuickSort(IList<int> arr, int low, int high)
         {
             if (high <= low)
@@ -26,6 +35,15 @@ namespace Algorithms.Sorting
             QuickSort(arr, pivot + 1, high);
         }
 
+        /// <summary>
+        /// Devides array (or part of array, depending from low and high indexes) into two parts. 
+        /// One part - all numbers that bigger than some pivot number,
+        /// second part - all numbers that smaller than this pivot number
+        /// </summary>
+        /// <param name="arr">Initial array of numbers</param>
+        /// <param name="low">Low index of array that need to be partioned</param>
+        /// <param name="high">High index of array that need to be partioned</param>
+        /// <returns>Index of pivot number (by which array was devided)</returns>
         public static int Partition(IList<int> arr, int low, int high)
         {
             int pivot = high;
@@ -45,6 +63,12 @@ namespace Algorithms.Sorting
             return less + 1;
         }
 
+        /// <summary>
+        /// Swaps two elements of array
+        /// </summary>
+        /// <param name="arr">array of integers</param>
+        /// <param name="i">index of first element to swipe</param>
+        /// <param name="j">index of second element to swipe</param>
         public static void Swap(IList<int> arr, int i, int j)
         {
             var temp = arr[i];
@@ -56,11 +80,20 @@ namespace Algorithms.Sorting
 
         #region Quick sort generic
 
+        /// <summary>
+        /// Quick sort of array
+        /// </summary>
         public static void QuickSort<T>(IList<T> arr) where T : IComparable
         {
             QuickSort(arr, 0, arr.Count - 1);
         }
 
+        /// <summary>
+        /// Recursive quick sort implementation
+        /// </summary>
+        /// <param name="arr">Initial array</param>
+        /// <param name="low">Low index of array from which it should be sorted</param>
+        /// <param name="high">High index of array till which it should be sorted</param>
         public static void QuickSort<T>(IList<T> arr, int low, int high) where T : IComparable
         {
             if (high <= low)
@@ -74,6 +107,15 @@ namespace Algorithms.Sorting
             QuickSort(arr, pivot + 1, high);
         }
 
+        /// <summary>
+        /// Devides array (or part of array, depending from low and high indexes) into two parts. 
+        /// One part - all objects that bigger than some pivot object,
+        /// second part - all objects that smaller than this pivot object
+        /// </summary>
+        /// <param name="arr">Initial array of objects</param>
+        /// <param name="low">Low index of array that need to be partioned</param>
+        /// <param name="high">High index of array that need to be partioned</param>
+        /// <returns>Index of pivot objects (by which array was devided)</returns>
         public static int Partition<T>(IList<T> arr, int low, int high) where T : IComparable
         {
             int pivot = high;
@@ -93,6 +135,12 @@ namespace Algorithms.Sorting
             return less + 1;
         }
 
+        /// <summary>
+        /// Swaps two elements of array
+        /// </summary>
+        /// <param name="arr">array of objects</param>
+        /// <param name="i">index of first element to swipe</param>
+        /// <param name="j">index of second element to swipe</param>
         public static void Swap<T>(IList<T> arr, int i, int j) where T : IComparable
         {
             var temp = arr[i];
@@ -104,6 +152,11 @@ namespace Algorithms.Sorting
 
         #region Extensions
 
+        /// <summary>
+        /// Enables to use quick sort by collections, like their own method
+        /// </summary>
+        /// <typeparam name="T">IComparable type</typeparam>
+        /// <param name="arr">array of IComparable objects</param>
         public static void QSort<T>(this IList<T> arr) where T : IComparable
         {
             QuickSort(arr, 0, arr.Count - 1);
