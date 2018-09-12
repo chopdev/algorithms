f5bbbcbee780cc1225158d8848d2f3a099533685
chopdev <taras.plavin@gmail.com>
2018-09-12 22:26:07 +0300

    add implementation for priority queue based of max heap

---
+++ DataStructures/Heap/PriorityQ.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataStructures.Heap
+{
+    public class PriorityQ
+    {
+        private List<int> arr;
+
+        public PriorityQ() {
+            arr = new List<int>();
+            arr.Add(default(int)); // fill redundant cell
+        }
+
+        public bool empty()
+        {
+            return arr.Count <= 1;
+        }
+
+        public void insert(int val) {
+            arr.Add(val);
+            int currInd = arr.Count - 1;
+
+            while (currInd > 1 && arr[currInd] > arr[currInd / 2]) {
+                swap(currInd, currInd / 2);
+                currInd = currInd / 2;
+            }
+        }
+
+        public int peek() {
+            if (empty()) throw new InvalidOperationException("Queue is empty");
+            return arr[1];
+        }
+
+        public int pop() {
+            if (empty()) throw new InvalidOperationException("Queue is empty");
+            int lastIndex = arr.Count - 1;
+            int result = arr[1];
+            swap(lastIndex, 1);
+            arr.RemoveAt(lastIndex);
+
+            int currInd = 1;
+            lastIndex = lastIndex - 1;
+
+            for (int leftInd = 2 * currInd, rightInd = 2 * currInd + 1;
+                leftInd <= lastIndex;
+                leftInd = 2 * currInd, rightInd = 2 * currInd + 1)
+            {
+                int left = leftInd <= lastIndex ? arr[leftInd] : int.MinValue;
+                int right = rightInd <= lastIndex ? arr[rightInd] : int.MinValue;
+
+                if (arr[currInd] > left && arr[currInd] > right) break;
+                if (left > right)
+                {
+                    swap(leftInd, currInd);
+                    currInd = leftInd;
+                }
+                else
+                {
+                    swap(rightInd, currInd);
+                    currInd = rightInd;
+                }
+            }
+
+            return result;
+        }
+
+        private void swap(int i, int j) {
+            int temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
+        }
+    }
+}
---
+++ XUnitTests/DataStructures/Heap/PriorityQTests.cs
@@ -0,0 +1,70 @@
+﻿using DataStructures.Heap;
+using System;
+using Xunit;
+
+namespace XUnitTests.DataStructures.Heap
+{
+    public class PriorityQTests
+    {
+        [Fact(DisplayName = "PriorityQ Insert Correct")]
+        public void PriorityQInsertCorrect()
+        {
+            PriorityQ queue = new PriorityQ();
+            queue.insert(1);
+            queue.insert(2);
+            queue.insert(3);
+            queue.insert(4);
+            queue.insert(5);
+            Assert.True(queue.peek() == 5);
+
+            queue = new PriorityQ();
+            queue.insert(5);
+            queue.insert(4);
+            queue.insert(3);
+            queue.insert(2);
+            queue.insert(1);
+            Assert.True(queue.peek() == 5);
+
+            queue = new PriorityQ();
+            queue.insert(3);
+            queue.insert(4);
+            queue.insert(5);
+            queue.insert(2);
+            queue.insert(1);
+            Assert.True(queue.peek() == 5);
+        }
+
+        [Fact(DisplayName = "PriorityQ Pop Correct")]
+        public void PriorityQPopCorrect()
+        {
+            PriorityQ queue = new PriorityQ();
+            queue.insert(3);
+            queue.insert(4);
+            queue.insert(5);
+            queue.insert(2);
+            queue.insert(1);
+
+            Assert.True(queue.pop() == 5);
+            Assert.True(queue.pop() == 4);
+            Assert.True(queue.pop() == 3);
+            Assert.True(queue.pop() == 2);
+            Assert.True(queue.pop() == 1);
+
+            queue = new PriorityQ();
+            queue.insert(3);
+            queue.insert(4);
+            queue.insert(5);
+            queue.insert(2);
+            queue.insert(1);
+            queue.insert(6);
+
+            Assert.True(queue.pop() == 6);
+            Assert.True(queue.pop() == 5);
+            Assert.True(queue.pop() == 4);
+            Assert.True(queue.pop() == 3);
+            Assert.True(queue.pop() == 2);
+            Assert.True(queue.pop() == 1);
+            Assert.Throws<InvalidOperationException>(() => queue.pop());
+        }
+    }
+}
