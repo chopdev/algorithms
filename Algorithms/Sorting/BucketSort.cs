efacab847657af6c746762ac78e13ea093a9a854
chopdev <taras.plavin@gmail.com>
2018-10-07 21:58:00 +0300

    add bucket sort

---
+++ Algorithms/Sorting/BucketSort.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Generic;
+
+namespace Algorithms.Sorting
+{
+    /// <summary>
+    /// Bucket sort is an efficient sorting with time complexity O(N). 
+    /// But it can be only applied to specific kind of data. 
+    /// It is useful when input is uniformly distributed over a range.
+    /// 
+    /// For example when the data - is array of ages of the people. People live max till 120-130
+    /// Another example: Sort a large set of floating point numbers which are in range from 0.0 to 1.0 and are uniformly distributed across the range. 
+    /// </summary>
+    public class BucketSort
+    {
+        /// <summary>
+        /// Sort set of floating point numbers which are in range from 0.0 to 1.0 and are uniformly distributed across the range. 
+        /// </summary>
+        public static void Sort(double[] arr, int n = 10)
+        {
+            var buckets = new List<double>[n];
+            for (int i = 0; i < arr.Length; i++)
+            {
+                int bucket = (int)(arr[i] * n);
+                if (buckets[bucket] == null)
+                    buckets[bucket] = new List<double>();
+                buckets[bucket].Add(arr[i]);
+            }
+
+            for (int i = 0; i < n; i++)
+                if(buckets[i] != null)
+                    buckets[i].Sort();
+
+            int index = 0;
+            for (int i = 0; i < n; i++)
+                for (int j = 0; buckets[i] != null && j < buckets[i].Count; j++)
+                    arr[index++] = buckets[i][j];
+        }
+    }
+}
---
+++ XUnitTests/Algorithms/BucketSortTests.cs
@@ -0,0 +1,21 @@
+﻿using Xunit;
+using Algorithms.Sorting;
+
+namespace XUnitTests.Algorithms
+{
+    public class BucketSortTests
+    {
+        [Fact(DisplayName = "Bucket sort works correct")]
+        public void BucketSortIsCorrect()
+        {
+            var arr = new double[] { 0.66, 0.668, 0.2, 0.33, 0.001, 0.1, 0.35, 0.78 };
+            BucketSort.Sort(arr);
+
+            var arrCorrect = new double[] { 0.001, 0.1, 0.2, 0.33, 0.35, 0.66, 0.668, 0.78 };
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Assert.Equal(arr[i], arrCorrect[i]);
+            }
+        }
+    }
+}
