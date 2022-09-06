05563f39a47c7efbc015791bd014da330c339b16
Taras Plavin <tpplavin@amazon.com>
2022-09-05 23:45:51 -0700

    add skeleton for Rabin Karp algo

---
+++ JAVA/src/Algorithms/RabinKarp.java
@@ -0,0 +1,14 @@
+package Algorithms;
+
+/**
+ * Rabin Karp substring search
+ * https://brilliant.org/wiki/rabin-karp-algorithm/
+ *
+ * A rolling hash allows an algorithm to calculate a hash value without having to rehash the entire string. For example,
+ * when searching for a word in a text, as the algorithm shifts one letter to the right (as in the animation for brute force),
+ * instead of having to calculate the hash of the section of text[1:3] as it shifts from text[0:2], the algorithm can use
+ * a rolling hash to do an operation on the hash to get the new hash from the old hash.
+ * */
+public class RabinKarp {
+    // todo: implement
+}
