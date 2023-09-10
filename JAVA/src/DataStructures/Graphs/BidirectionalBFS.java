8f3cd5676eb2e900e0708a269a859e7b7d13ec85
Taras Plavin <tpplavin@amazon.com>
2023-09-10 01:11:00 -0700

    add template for bidirectional BFS

---
+++ JAVA/src/DataStructures/Graphs/BidirectionalBFS.java
@@ -0,0 +1,5 @@
+package DataStructures.Graphs;
+
+public class BidirectionalBFS {
+    // TODO: https://github.com/chopdev/leetcode_tasks/blob/master/graph/_1197_minimum_knight_moves/Solution.java
+}
